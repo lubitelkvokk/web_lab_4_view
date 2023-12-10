f2570b3059036610d77e3e192e88415d1366d676
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2023-12-11 01:24:15 +0300

    added proxy for fixing CORS and logging in auth.service with output in login.component

--- src/app/account_manager/auth.module.ts
+++ src/app/account_manager/auth.module.ts
@@ -3,16 +3,18 @@ import {CommonModule} from "@angular/common";
 import {AuthRoutingModule} from "./auth-routing.module";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent
   ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-  ],
+    imports: [
+        CommonModule,
+        AuthRoutingModule,
+        FormsModule,
+    ],
   exports: [
     LoginComponent,
     RegisterComponent
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,34 +1,53 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { map } from "rxjs/operators";
-import { JwtHelperService } from "@auth0/angular-jwt";
-import { User } from "../../models/user";
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {User} from "../../models/user";
+import {catchError, map} from "rxjs";
+import {Response} from "../../models/response";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json;',
+  }),
+  // Добавляем режим 'no-cors'
+  mode: 'no-cors'
+};
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  authUrl = "http://localhost:5000/api/user/";
+  authUrl = "http://localhost:8080/web_app_lab_4_war_exploded/api/user";
 
-  constructor(private http: HttpClient) {}
+  isAuth = false;
 
-  login(model: any) {
-
+  constructor(private http: HttpClient) {
   }
+
+  login(model: User) {
+    return this.http.post(this.authUrl, model, {responseType: 'text'}).pipe(
+      map((data)=>{
+        this.isAuth = true;
+        return new Response(200, data);
+      }),
+      catchError((err) => {
+        this.isAuth = false;
+        return [new Response(404, err.error)];
+      })
+    );
+      // .subscribe({
+      //   next: (data: any) => {
+      //     this.isAuth = true;
+      //     return new Response(200, data);
+      //   },
+      //   error: (error) => {
+      //     this.isAuth = false;
+      //     return new Response(error.code, error.error);
+      //   }
+      // });
+  }
+
 
   register(model: any) {
-
-  }
-
-  resetPassword(model: any) {
-
-  }
-
-  confirmEmail(model: any) {
-
-  }
-
-  changePassword(model: any) {
 
   }
 
